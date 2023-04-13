b6de018a98a7350cca48bf35b034bb9a329729cd
Ayman Mohamed Elashry <102562894+engayman2002@users.noreply.github.com>
2023-04-13 03:06:41 +0200

    Update Task5.c

--- Pointer Assignment/Task5.c
+++ Pointer Assignment/Task5.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
-	int x,y,z;
-	int* px,py,pz;
-	x=10,y=20,z=30;
-	px=&x,py=&y,pz=&z;
-	// a- Print with labels the values of x, y, z, px, py, pz, *px, *py, *pz.
+    int x,y,z;
+    int *px,*py,*pz;
+    x=5,y=10,z=15;
+    px=&x,py=&y,pz=&z;
+    // A
+    printf("x=%d y=%d z=%d px=%p py=%p pz=%p *px=%d *py=%d *pz=%d",x,y,z,px,py,pz,*px,*py,*pz);
+    // B
+    printf("\nSwapping pointers\n");
+    // C
+    int* tz=pz;
+    pz=px;
+    px=py;
+    py=tz;
+    // D
+    printf("x=%d y=%d z=%d px=%p py=%p pz=%p *px=%d *py=%d *pz=%d",x,y,z,px,py,pz,*px,*py,*pz);
 
     return 0;
  }
